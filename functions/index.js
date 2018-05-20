d734867a11d4bdc04db91585f2727c4938ee7ea6
Diego Villalobos <diegovillalobos397@gmail.com>
2018-05-20 03:40:23 -0500

    Added get Announcements functionality

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,8 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const cors = require('cors')({origin: true});
+
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +10,22 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.getAnnouncements = functions.https.onRequest((req,res) => {
+    cors(req,res, () => {
+        admin.firestore().collection('anuncios').orderBy('fecha','desc').get()
+            .then(anounciosSnapshot => {
+                const anuncios = [];
+                anounciosSnapshot.forEach(anuncioRef => {
+                    const anuncio = anuncioRef.data();
+                    anuncio.id = anuncioRef.id;
+                    anuncios.push(anuncio);
+                });
+                return res.status(200).send(anuncios)
+            })
+            .catch(error => {
+                console.log(error);
+                return res.status(400).send(error)
+            })
+    })
+});
