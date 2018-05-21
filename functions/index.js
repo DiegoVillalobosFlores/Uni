70aec22c35d24a7765a047748dafb8e5cfb7649e
Diego Villalobos <diegovillalobos397@gmail.com>
2018-05-21 01:13:05 -0500

    Added post Comment functionality Added get comments functionality

--- functions/index.js
+++ functions/index.js
@@ -13,15 +13,65 @@ admin.initializeApp();
 
 exports.getAnnouncements = functions.https.onRequest((req,res) => {
     cors(req,res, () => {
-        admin.firestore().collection('anuncios').orderBy('fecha','desc').get()
+        const anuncios = [];
+        const anunciosRef = admin.firestore().collection('anuncios');
+        anunciosRef.orderBy('fecha','desc').get()
             .then(anounciosSnapshot => {
-                const anuncios = [];
                 anounciosSnapshot.forEach(anuncioRef => {
                     const anuncio = anuncioRef.data();
                     anuncio.id = anuncioRef.id;
                     anuncios.push(anuncio);
                 });
                 return res.status(200).send(anuncios)
+            })
+            .catch(error => {
+                console.log(error);
+                return res.status(400).send(error)
+            })
+    })
+});
+
+exports.getAnnouncementComment = functions.https.onRequest((req,res) => {
+    const data = {
+        announcement: req.query.announcement
+    };
+
+    cors(req,res,() => {
+        return admin.firestore().collection('anuncios').doc(data.announcement).collection('comments').get()
+            .then(commentsSnapshot => {
+                const comments = [];
+                commentsSnapshot.forEach(commentRef => {
+                    const comment = commentRef.data();
+                    comment.id = commentRef.id;
+                    comments.push(comment)
+                });
+                return res.status(200).send(comments)
+            })
+            .catch(error => {
+                console.log(error);
+                return res.status(400).send(error)
+            })
+    })
+});
+
+exports.postComment = functions.https.onRequest((req,res) => {
+
+
+    console.log("BODY",req.body);
+    const data = {
+        comment: req.body.comment,
+        announcement: req.query.announcement
+    };
+
+
+    console.log("DATA",data);
+
+    data.comment.date = new Date();
+
+    cors(req,res,() => {
+        admin.firestore().collection('anuncios').doc(data.announcement).collection('comments').add(data.comment)
+            .then(response => {
+                return res.status(200).send({ok: response.id})
             })
             .catch(error => {
                 console.log(error);
